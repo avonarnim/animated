224bd20cadf6cc57f9b51b34f370da1fe8e5139a
Adam von Arnim <vonarnim@usc.edu>
2022-04-08 00:56:24 -0700

    bezier inlaid circles

--- js/animateLine.js
+++ js/animateLine.js
@@ -1,5 +1,5 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var diagCanvas = document.getElementById("diagonalCanvas");
+var ctx = diagCanvas.getContext("2d");
 ctx.moveTo(0, 0);
 ctx.lineTo(200, 100);
 ctx.stroke();
