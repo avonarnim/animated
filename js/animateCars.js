632a039b463f9d893a91e4a25604ab8d47d71873
Adam von Arnim <vonarnim@usc.edu>
2022-04-09 10:59:31 -0700

    varying cars with bounds

--- js/animateCars.js
+++ js/animateCars.js
@@ -4,7 +4,8 @@ var ctx = carCanvas.getContext("2d");
 carCanvas.height = innerHeight - 20;
 carCanvas.width = innerWidth - 20;
 
-let carPosX = 0;
+let carPosX = [75, 500, 200, 0];
+let carMultipliers = [0.5, 0.2, 0.4, 0.6];
 
 addEventListener("resize", () => {
   carCanvas.height = innerHeight - 20;
@@ -21,11 +22,26 @@ img.onload = () => {
 };
 
 carCanvas.addEventListener("wheel", (e) => {
-  carPosX += e.deltaY * 0.5;
+  carPosX[0] += e.deltaY * carMultipliers[0];
+  carPosX[1] += e.deltaY * carMultipliers[1];
+  carPosX[2] += e.deltaY * carMultipliers[2];
+  carPosX[3] += e.deltaY * carMultipliers[3];
+
+  carPosX.forEach((pos, i) => {
+    if (pos > carCanvas.width - 250) {
+      carPosX[i] = carCanvas.width - 250;
+    } else if (pos < 0) {
+      carPosX[i] = 0;
+    }
+  });
+
   animate();
 });
 
 function animate() {
   ctx.clearRect(0, 0, carCanvas.width, carCanvas.height);
-  ctx.drawImage(img, carPosX, 5);
+  ctx.drawImage(img, carPosX[0], 5);
+  ctx.drawImage(img, carPosX[1], 125);
+  ctx.drawImage(img, carPosX[2], 245);
+  ctx.drawImage(img, carPosX[3], 365);
 }
